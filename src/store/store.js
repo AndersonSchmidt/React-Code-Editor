1c0fc6443e68d3d440e317a2ac5a793f015b5ed6
Anderson Schmidt <schmidtanderson@hotmail.com>
2020-08-23 18:54:15 -0300

    Adding filesReducer on rootReducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import ReduxThunk from "redux-thunk";
 import filetreeReducer from "./reducers/filetree";
+import filesReducer from "./reducers/files";
 
 const rootReducer = combineReducers({
   filetree: filetreeReducer,
+  files: filesReducer,
 });
 
 const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
