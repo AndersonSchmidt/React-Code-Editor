17bba0e90ae0ebaeb3444bab48b8e4c40b1572b7
Anderson Schmidt <schmidtanderson@hotmail.com>
2020-08-26 12:49:52 -0300

    Adding expand and collapse folder functionality

--- src/components/Sidebar/File/File.js
+++ src/components/Sidebar/File/File.js
@@ -1,23 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./File.module.css";
 import { useDispatch } from "react-redux";
 import * as filesActions from "../../../store/actions/files";
 
 const File = (props) => {
   const dispatch = useDispatch();
+  const [show, setShow] = useState(true);
+  const file = props.file;
+  const isDirectory = props.file.isDirectory;
+  const children = props.file.children || [];
 
   const fetchFile = async () => {
     await dispatch(filesActions.fetchFile(props.file.id));
   };
 
+  const toggleShow = () => {
+    setShow(!show);
+  };
+
   return (
     <div className={classes.Container}>
-      <p onClick={fetchFile}>{props.file.name}</p>
-      {props.file.children
-        ? props.file.children.map((file) => {
+      <p onClick={isDirectory ? toggleShow : fetchFile}>{file.name}</p>
+      {show ? (
+        <div>
+          {children.map((file) => {
             return <File file={file} key={file.id} />;
-          })
-        : null}
+          })}
+        </div>
+      ) : null}
     </div>
   );
 };
