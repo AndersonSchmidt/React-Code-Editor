a0b761bfc824d26612419c55bc57e063cfe469e1
Anderson Schmidt <schmidtanderson@hotmail.com>
2020-08-26 21:29:54 -0300

    Adding react-monaco-editor

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
+import MonacoEditor from "react-monaco-editor";
 
 const Editor = (props) => {
   const file = useSelector((state) => state.files.file);
-  return <p>{file.content}</p>;
+  const [content, setContent] = useState("");
+
+  const onChangeHandler = (newValue) => {
+    setContent(newValue);
+  };
+
+  useEffect(() => {
+    setContent(file.content);
+  }, [file]);
+
+  return (
+    <MonacoEditor
+      width="800"
+      height="600"
+      language="java"
+      theme="vs-dark"
+      value={content}
+      options={{
+        selectOnLineNumbers: true,
+      }}
+      onChange={onChangeHandler}
+    />
+  );
 };
 
 export default Editor;
